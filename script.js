f5031349e9cf74870400d5be76a242927b41125c
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-10-20 10:13:24 +0400

    date_difference

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function dateTime(){
     let date = new Date().toLocaleString('az-Az');
     document.getElementById('date').innerHTML = date;
+    let date1 = new Date("08/11/1990").toLocaleString('az-AZ');
+    document.getElementById('date1').innerHTML = date1;
+    
 }
 setInterval(dateTime, 1000);
+
+
+
+    
+
+
