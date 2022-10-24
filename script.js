29af8126a9729067babf83dd7a02bf8dc3c0b3bb
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-10-24 08:27:50 +0400

    the number of days you have lived

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function dateTime(){
-    let date = new Date().toLocaleString('az-Az');
-    document.getElementById('date').innerHTML = date;
-    let date1 = new Date("08/11/1990").toLocaleString('az-AZ');
-    document.getElementById('date1').innerHTML = date1;
-    
-}
-setInterval(dateTime, 1000);
+var yourDate=prompt("Enter your birthday as yyyy-mm-dd")
+let date1=new Date(yourDate);
+let currentDate = new Date().toJSON().slice(0, 10);
+//.toJSON() bele bir tarix setri verir 2022-06-17T11:06:50.369Z ona gore ilk 10 elementi gotururuk
+let date2=new Date(currentDate);
+let difference=date2.getTime()-date1.getTime();
+console.log(difference/1000/(24*60*60)+" days")
 
 
 
     
-
-
